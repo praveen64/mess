f642ba29b965028278a2152a8fb1786ffff849d1
Praveen Gadikoppula <praveen.androids@gmail.com>
2018-08-20 15:20:24 +0530

    db config

--- config.php
+++ config.php
@@ -1,7 +1,7 @@
 <?php
 $mysql_hostname = "localhost";
-$mysql_user = "root";
-$mysql_password = "1234";
+$mysql_user = "praveen";
+$mysql_password = "p1p2p364";
 $mysql_database = "mess";
 $bd = mysql_connect($mysql_hostname, $mysql_user, $mysql_password) or die("Could not connect database");
 mysql_select_db($mysql_database, $bd) or die("Could not select database");
